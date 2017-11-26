00991d89ee541bd3f9e569319a7f457128c29372
Lorenz <lorenzkummer@gmx.at>
2017-11-26 18:52:04 +0100

    PLC Project 2

---
+++ src/main/scala/MainApp.scala
@@ -0,0 +1,31 @@
+import EuroConverter._
+import BitcoinConverter._
+import DollarConverter._
+import Printer._
+import akka.actor.{ActorRef, ActorSystem}
+
+object MainApp extends App {
+
+  val system: ActorSystem = ActorSystem("mainAkka")
+
+  try {
+    val printer: ActorRef = system.actorOf(Printer.props, "printerActor")
+    val ec: ActorRef = system.actorOf(EuroConverter.props(printer), "euroConverter")
+    val bc: ActorRef = system.actorOf(BitcoinConverter.props(printer), "bitcoinConverter")
+    val dc: ActorRef = system.actorOf(DollarConverter.props(printer), "dollarConverter")
+
+    val bitcoin = BigDecimal("1")
+    val dollar = BigDecimal("200")
+    val euro = BigDecimal("300")
+
+    bc ! bitcoin2dollar(bitcoin)
+    bc ! bitcoin2euro(bitcoin)
+    dc ! dollar2euro(dollar)
+    dc ! dollar2bitcoin(dollar)
+    ec ! euro2dollar(euro)
+    ec ! euro2bitcoin(euro)
+
+  } finally {
+    system.terminate()
+  }
+}
